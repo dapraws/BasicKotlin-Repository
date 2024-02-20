d105cfbf87ed992de1e974269e7aebe4fb6d692b
dapraws <darrelprawira26@gmail.com>
2024-02-20 22:51:41 +0700

    class

---
+++ src/main/kotlin/class.kt
@@ -0,0 +1,39 @@
+package com.dicoding.kotlin
+
+class Animal() {
+    var name: String = "Kucing"
+    var weight: Double = 3.2
+    var age: Int = 2
+    var isMammal: Boolean = true
+
+    fun eat(){
+        println("$name makan!")
+    }
+
+    fun sleep() {
+        println("$name tidur!")
+    }
+}
+
+fun main() {
+    val dicodingCat = Animal()
+    println("Nama: ${dicodingCat.name}, Berat: ${dicodingCat.weight}, Umur: ${dicodingCat.age}, mamalia: ${dicodingCat.isMammal}" )
+    dicodingCat.eat()
+    dicodingCat.sleep()
+
+// Mengubah Properti dari class dicodingCat
+    dicodingCat.name = "Kucing Oren"
+    dicodingCat.weight = 6.0
+    dicodingCat.age = 3
+    println("Nama: ${dicodingCat.name}, Berat: ${dicodingCat.weight}, Umur: ${dicodingCat.age}, mamalia: ${dicodingCat.isMammal}" )
+    dicodingCat.eat()
+    dicodingCat.sleep()
+}
+/*
+    Nama: Kucing, Berat: 3.2, Umur: 2, mamalia: true
+    Kucing makan!
+    Kucing tidur!
+    Nama: Kucing Oren, Berat: 6.0, Umur: 3, mamalia: true
+    Kucing Oren makan!
+    Kucing Oren tidur!
+*/
