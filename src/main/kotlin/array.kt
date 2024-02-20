1f5741213a2e9b62a65eff8a97f2da8a628c01c8
dapraws <darrelprawira26@gmail.com>
2024-02-20 20:45:34 +0700

    array

--- src/main/kotlin/array.kt
+++ src/main/kotlin/array.kt
@@ -1,2 +1,22 @@
 package com.dicoding.kotlin
 
+fun main() {
+    val array = arrayOf(1, 3, 5, 7)
+    val mixArray = arrayOf(1, 3, 5, 7 , "Dicoding" , true)
+
+    val intArray = intArrayOf(1, 3, 5, 7)
+    /*
+        intArrayOf() : IntArray
+        booleanArrayOf() : BooleanArray
+        charArrayOf() : CharArray
+        longArrayOf() : LongArray
+        shortArrayOf() : ShortArray
+        byteArrayOf() : ByteArray
+    */
+
+//    Indexing
+    print(intArray[2]) // 5
+
+    intArray[2] = 11
+    print(intArray[2]) // 11
+}
