caf544d5ab3f2985fd7800f6c6e5684c8e6e0f02
dapraws <darrelprawira26@gmail.com>
2024-02-20 22:33:40 +0700

    while and do-while

---
+++ src/main/kotlin/whiledowhile.kt
@@ -0,0 +1,37 @@
+package com.dicoding.kotlin
+
+fun main() {
+//    While
+    var counter = 1
+    while (counter <= 7){
+        println("Hello, World!")
+        counter++
+    }
+    /*
+       output :
+           Hello, World!
+           Hello, World!
+           Hello, World!
+           Hello, World!
+           Hello, World!
+           Hello, World!
+           Hello, World!
+    */
+
+//    Do While
+    do {
+        println("Hello, World!")
+        counter++
+    } while (counter <= 7)
+    /*
+       output:
+           Hello, World!
+           Hello, World!
+           Hello, World!
+           Hello, World!
+           Hello, World!
+           Hello, World!
+           Hello, World!
+    */
+
+}
