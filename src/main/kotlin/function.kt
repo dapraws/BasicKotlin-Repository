6a61f9d0f949783c374a2891da4843903d50318c
dapraws <darrelprawira26@gmail.com>
2024-02-20 21:13:54 +0700

    function

--- src/main/kotlin/function.kt
+++ src/main/kotlin/function.kt
@@ -8,11 +8,31 @@ fun printUser(name: String) {
     print("Your name is $name")
 }
 
+fun getFullName1(first: String, middle: String, last: String): String {
+    return "$first $middle $last"
+}
+
+fun getFullName2(
+    first: String = "Kotlin",
+    middle: String = " is ",
+    last: String = "Awesome"): String {
+    return "$first $middle $last"
+}
+
 fun main() {
     val user = setUser("Alfian", 19)
     println(user)
 
     printUser("Alfian")
+
+//    Named Argument
+    val fullName1 = getFullName1(first = "Kotlin" , middle = " is ", last = "Fantastic")
+    print(fullName1) // // Kotlin is Fantastic
+
+//    Default Argument
+    val fullName2 = getFullName2()
+    print(fullName2) // Kotlin is Awesome
+
 }
 
 /*
