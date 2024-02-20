16f8300130737e74d12c16e04a9945a425f0fb80
dapraws <darrelprawira26@gmail.com>
2024-02-20 20:59:42 +0700

    nullable types

--- src/main/kotlin/nullabletypes.kt
+++ src/main/kotlin/nullabletypes.kt
@@ -2,4 +2,11 @@ package com.dicoding.kotlin
 
 fun main() {
     val text: String? = null
+
+//    Safe calls
+    text?.length
+
+//    Elvis Operator (condition ? then : else)
+    val textLength = text?.length ?: 7 // sama seperti val textLength = if (text != null) text.length else 7
+
 }
