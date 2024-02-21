e687043a150755dc0b6e576fd243fe0b396be913
dapraws <darrelprawira26@gmail.com>
2024-02-22 00:18:48 +0700

    function type

---
+++ src/main/kotlin/functiontype.kt
@@ -0,0 +1,11 @@
+package com.dicoding.kotlin
+
+typealias Arithmetic = ((Int, Int) -> Int)?
+
+fun main() {
+    val sum: Arithmetic = { valueA, valueB -> valueA + valueB }
+
+    val sumResult = sum?.invoke(10, 10)
+
+    println(sumResult)
+}
