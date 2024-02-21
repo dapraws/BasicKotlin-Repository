7ba8db5f1451040a921a8d70caf73866e35553c2
dapraws <darrelprawira26@gmail.com>
2024-02-21 23:40:32 +0700

    sequence

---
+++ src/main/kotlin/sequence.kt
@@ -0,0 +1,13 @@
+package com.dicoding.kotlin
+
+fun main () {
+    val list = (1..10000).toList()
+    val number = list.asSequence().filter { it % 5 == 0 }.map { it * 2 }.first()
+    println(number)
+    // Output: 10
+
+
+    val sequenceNumber = generateSequence(1) { it + 1 }
+    sequenceNumber.take(5).forEach { print("$it ") }
+    // Output: 1 2 3 4 5
+}
