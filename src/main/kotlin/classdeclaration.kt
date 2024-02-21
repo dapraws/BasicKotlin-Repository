18cded7121a239e3a9ef1a243acca268f595c3e9
dapraws <darrelprawira26@gmail.com>
2024-02-22 01:10:39 +0700

    class declaration

---
+++ src/main/kotlin/classdeclaration.kt
@@ -0,0 +1,19 @@
+package com.dicoding.kotlin
+
+class LongList : List<Long>{
+    override fun get(index: Int): Long {
+        return this[index]
+    }
+}
+class ArrayList<T> : List<T>{
+    override fun get(index: Int): T {
+        return this[index]
+    }
+}
+interface List<P> {
+    operator fun get(index: Int): P
+}
+fun main() {
+    val longArrayList = ArrayList<Long>()
+    val firstLong = longArrayList.get(0)
+}
