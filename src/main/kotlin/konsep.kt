77de05131f163e1b7a45ce4c0970fbc454b0e736
dapraws <darrelprawira26@gmail.com>
2024-02-22 00:18:06 +0700

    konsep

---
+++ src/main/kotlin/konsep.kt
@@ -0,0 +1,20 @@
+package com.dicoding.kotlin
+
+fun main() {
+//    val list = getListUser()
+
+//    fun getUsername(): List<String>{
+//        val name = mutableListOf<String>()
+//        for (user in list){
+//            name.add(user.name)
+//        }
+//        return name
+//    }
+
+    // Karena kode pada Kotlin bisa dituliskan dengan gaya fungsional, maka kode di atas cukup dituliskan seperti berikut:
+//    fun getUsername(): List<String>{
+//        return list.map {
+//            it.name
+//        }
+//    }
+}
