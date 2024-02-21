38bd7457bebd1cdbfb1a6f5dc543192691528391
dapraws <darrelprawira26@gmail.com>
2024-02-21 17:30:57 +0700

    interface

---
+++ src/main/kotlin/interface.kt
@@ -0,0 +1,13 @@
+package com.dicoding.kotlin
+
+interface IFly {
+    fun fly()
+    val numberOfWings: Int
+}
+
+class Bird(override val numberOfWings: Int) : IFly {
+    override fun fly() {
+        if(numberOfWings > 0) println("Flying with $numberOfWings wings")
+        else println("I'm Flying without wings")
+    }
+}
