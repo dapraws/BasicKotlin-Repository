457a150cf6a2514a40ca191bf59e19969e787792
dapraws <darrelprawira26@gmail.com>
2024-02-22 00:53:55 +0700

    recursion

---
+++ src/main/kotlin/recursion.kt
@@ -0,0 +1,15 @@
+package com.dicoding.kotlin
+
+fun main() {
+    println("Factorial 9999 is: ${factorial(9999)}")
+}
+
+
+tailrec fun factorial(n: Int, result: Int = 1): Int {
+    val newResult = n * result
+    return if (n == 1) {
+        newResult
+    } else {
+        factorial(n - 1, newResult)
+    }
+}
