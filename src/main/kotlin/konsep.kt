a25e162471e71b620d5fb690ac72c1adc917b92e
dapraws <darrelprawira26@gmail.com>
2024-02-22 01:27:35 +0700

    add dependencies

--- src/main/kotlin/konsep.kt
+++
@@ -1,6 +0,0 @@
-package com.dicoding.kotlin
-
-//val points = mapOf<String, Int>( "alfian" to 10 , "dimas" to 20 )
-
-//bisa disingkat menjadi seperti ini
-val points = mapOf("alfian" to 10, "dimas" to 20)
