ebce84976be205aaf164f45a2595d68a2a14289f
dapraws <darrelprawira26@gmail.com>
2024-02-21 18:11:31 +0700

    exception and handling

--- src/main/kotlin/exception.kt
+++ src/main/kotlin/exception.kt
@@ -1,12 +1,41 @@
 package com.dicoding.kotlin
 
+
+// Exception
+//fun main() {
+//    val someNullValue: String? = null
+//    val someMustNotNullValue: String = someNullValue!!
+//    println(someMustNotNullValue)
+//}
+//
+///*
+//output:
+//    Exception in thread "main" kotlin.NullPointerException at MainKt.main(Main.kt:3)
+//*/
+
+// Exception Handling
+import kotlin.NumberFormatException
+
 fun main() {
-    val someNullValue: String? = null
-    val someMustNotNullValue: String = someNullValue!!
-    println(someMustNotNullValue)
+    val someStringValue: String? = null
+    var someIntValue: Int = 0
+
+    try {
+        someIntValue = someStringValue!!.toInt()
+    } catch (e: NullPointerException) {
+        someIntValue = 0
+    } catch (e: NumberFormatException) {
+        someIntValue = -1
+    } finally {
+        when(someIntValue){
+            0 -> println("Catch block NullPointerException terpanggil !")
+            -1 -> println("Catch block NumberFormatException terpanggil !")
+            else -> println(someIntValue)
+        }
+    }
 }
 
 /*
-output:
-    Exception in thread "main" kotlin.NullPointerException at MainKt.main(Main.kt:3)
+	output:
+    Catch block NullPointerException terpanggil!
 */
